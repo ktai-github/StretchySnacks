cd789900ce4c1f341ab91f6a73b29ede82c6baae
Kevin <kevin.tai.mail@gmail.com>
2018-03-29 14:08:28 -0700

    added top view

---
+++ StretchySnacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchySnacks
+//
+//  Created by KevinT on 2018-03-29.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
