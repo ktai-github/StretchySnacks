7db35f9eb7124065be13de21420692361fb92d55
Kevin <kevin.tai.mail@gmail.com>
2018-03-29 16:50:34 -0700

    rotating plus button

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var plusButton: UIButton!
   @IBOutlet weak var statusView: UIView!
   @IBOutlet weak var heightConstraint: NSLayoutConstraint!
   
@@ -28,8 +29,29 @@ class ViewController: UIViewController {
     
     if self.heightConstraint.constant == CGFloat(200) {
       self.heightConstraint.constant = CGFloat(60)
+      let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+      animation.duration = 0.5
+      animation.fromValue = -0.75
+      animation.toValue = 0
+      plusButton.layer.add(animation, forKey: "transform.rotation.z")
+      
+      UIView.animate(withDuration: 0.75, delay: 0.5, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.3, options: [], animations: {
+        self.heightConstraint.constant = CGFloat(66)
+        self.view.layoutIfNeeded()
+        
+      })
+
       self.view.layoutIfNeeded()
+      return
     }
+    
+    let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+    animation.duration = 0.5
+    animation.fromValue = 0
+    animation.toValue = -0.75
+    animation.fillMode = kCAFillModeForwards;
+    animation.isRemovedOnCompletion = false;
+    plusButton.layer.add(animation, forKey: "transform.rotation.z")
     
     UIView.animate(withDuration: 0.75, delay: 0.5, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.3, options: [], animations: {
       self.heightConstraint.constant = CGFloat(200)
