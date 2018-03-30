c770ad7886dacf569a802cb3923a0e8ef125d02c
Kevin <kevin.tai.mail@gmail.com>
2018-03-29 19:50:19 -0700

    snack hidden and sized properly

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
   @IBOutlet weak var plusButton: UIButton!
   @IBOutlet weak var statusView: UIView!
   @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+  @IBOutlet weak var snacksStackView: UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,37 +28,68 @@ class ViewController: UIViewController {
   @IBAction func plusButtonTouched(_ sender: UIButton) {
     print("plus button pressed")
     
-    if self.heightConstraint.constant == CGFloat(200) {
-      self.heightConstraint.constant = CGFloat(60)
-      let animation = CABasicAnimation(keyPath: "transform.rotation.z")
-      animation.duration = 0.5
-      animation.fromValue = -0.75
-      animation.toValue = 0
-      plusButton.layer.add(animation, forKey: "transform.rotation.z")
-      
-      UIView.animate(withDuration: 0.75, delay: 0.5, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.3, options: [], animations: {
-        self.heightConstraint.constant = CGFloat(66)
-        self.view.layoutIfNeeded()
-        
-      })
-
-      self.view.layoutIfNeeded()
-      return
+    let closedValue:CGFloat = 66
+    let openValue:CGFloat = 200
+    
+    let rotated:CGFloat = -0.75
+    let unrotated:CGFloat = 0
+    
+    var isNavClosed:Bool
+    
+    if self.heightConstraint.constant == closedValue {
+      isNavClosed = true
+    }else{
+      isNavClosed = false
     }
+    
+    let newValue:CGFloat = isNavClosed ? openValue : closedValue
+    
+//    if self.heightConstraint.constant == CGFloat(200) {
+//      self.heightConstraint.constant = CGFloat(66)
+//      let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+//      animation.duration = 0.5
+//      animation.fromValue = -0.75
+//      animation.toValue = 0
+//      plusButton.layer.add(animation, forKey: "transform.rotation.z")
+//
+//      UIView.animate(withDuration: 0.75, delay: 0.5, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.3, options: [], animations: {
+//        self.heightConstraint.constant = newValue
+//        self.view.layoutIfNeeded()
+//
+//      })
+//
+//      self.view.layoutIfNeeded()
+//      return
+//    }
     
     let animation = CABasicAnimation(keyPath: "transform.rotation.z")
     animation.duration = 0.5
-    animation.fromValue = 0
-    animation.toValue = -0.75
+    
+    let newAngle:CGFloat = isNavClosed ? rotated : unrotated
+    let oldAngle:CGFloat = isNavClosed ? rotated : unrotated
+
+    animation.fromValue = oldAngle
+    animation.toValue = newAngle
     animation.fillMode = kCAFillModeForwards;
     animation.isRemovedOnCompletion = false;
     plusButton.layer.add(animation, forKey: "transform.rotation.z")
     
+//    let areImagesHidden:Bool = isNavClosed ? true : false
+//    snacksStackView.isHidden = areImagesHidden
+//    if snacksStackView.isHidden == true {
+//      snacksStackView.isHidden = false
+//    } else {
+//      snacksStackView.isHidden = true
+//    }
+    
+    snacksStackView.isHidden = !snacksStackView.isHidden
+    
     UIView.animate(withDuration: 0.75, delay: 0.5, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.3, options: [], animations: {
-      self.heightConstraint.constant = CGFloat(200)
+      self.heightConstraint.constant = newValue
       self.view.layoutIfNeeded()
-
     })
+    
+
   }
   
 }
